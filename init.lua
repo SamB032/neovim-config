e92d66c1045920b968fc9c292720dd66113bb96a
Sam <70594311+SamB032@users.noreply.github.com>
2024-07-28 14:42:04 +0000

    Add inital config settings from workset up

---
+++ init.lua
@@ -0,0 +1,100 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+require('move').setup({
+	line = {
+		enable = true, -- Enables line movement
+	},
+	block = {
+		enable = true, -- Enables block movement
+	},
+	word = {
+		enable = true, -- Enables word movement
+	},
+	char = {
+		enable = false -- Enables char movement
+	}
+})
+
+
+
+function comment_out()
+  vim.api.nvim_command("s:^:#:")
+  vim.api.nvim_command("noh")
+end
+
+vim.opt.shell = "powershell.exe"
+
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- This is the default configuration
+require('guess-indent').setup {
+  auto_cmd = true,  -- Set to false to disable automatic execution
+  override_editorconfig = false, -- Set to true to override settings set by .editorconfig
+  filetype_exclude = {  -- A list of filetypes for which the auto command gets disabled
+    "netrw",
+    "tutor",
+  },
+  buftype_exclude = {  -- A list of buffer types for which the auto command gets disabled
+    "help",
+    "nofile",
+    "terminal",
+    "prompt",
+  },
+  on_tab_options = { -- A table of vim options when tabs are detected 
+    ["expandtab"] = false,
+  },
+  on_space_options = { -- A table of vim options when spaces are detected 
+    ["expandtab"] = true,
+    ["tabstop"] = "detected", -- If the option value is 'detected', The value is set to the automatically detected indent size.
+    ["softtabstop"] = "detected",
+    ["shiftwidth"] = "detected",
+  },
+}
+
