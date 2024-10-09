67007f496eff5d93569c291f09b60097f5244008
Sam <sam.boffey04@gmail.com>
2024-10-09 20:39:23 +0100

    Add lazygit to config

--- init.lua
+++ init.lua
@@ -37,9 +37,19 @@ vim.schedule(function()
   require "mappings"
 end)
 
+-- Powershell config settings
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
 
-vim.opt.shell = "powershell.exe"
-
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
