212a03b81e056c086ee1e13b75b4a33398babf38
Sam <sam.boffey04@gmail.com>
2024-10-12 10:53:07 +0100

    Remove todo-comments.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,7 +23,11 @@ return {
   {
     "NMAC427/guess-indent.nvim",
   },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {}
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -61,21 +65,6 @@ return {
         "<leader>xX",
         "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
         desc = "Buffer Diagnostics (Trouble)",
-     },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
       },
       {
         "<leader>xQ",
@@ -104,9 +93,9 @@ return {
         { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   },
-  { 
-    'akinsho/toggleterm.nvim', 
-    version = "*", 
-    config = true 
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true
   },
 }
